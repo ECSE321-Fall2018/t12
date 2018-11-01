57e30c8f52a6d47bce997feba65285e0f7e7fbd6
Karl Godin <godinkarl@gmail.com>
2018-11-01 00:36:54 -0400

    Driver recycler view for my trips and bug fixes

--- Driver-app/app/src/main/java/ca/mcgill/ecse321/driverapp/MyTripsActivity.java
+++ Driver-app/app/src/main/java/ca/mcgill/ecse321/driverapp/MyTripsActivity.java
@@ -1,13 +1,25 @@
 package ca.mcgill.ecse321.driverapp;
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 
-public class MyTripsActivity extends AppCompatActivity {
+import java.sql.Time;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import ca.mcgill.ecse321.driverapp.adapters.TripAdapter;
+import ca.mcgill.ecse321.driverapp.model.Trip;
+
+public class MyTripsActivity extends AppCompatActivity  implements TripAdapter.ItemClickListener{
+    private RecyclerView myTripsView;
+
+    private TripAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,14 +28,53 @@ public class MyTripsActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        myTripsView = (RecyclerView) findViewById(R.id.MyTripsView);
+
+        populateTrips();
     }
 
+    private void populateTrips(){
+        RecyclerView.LayoutManager lm = new LinearLayoutManager(this);
+        adapter = new TripAdapter(this, htmlGetTrips());
+
+        myTripsView.setLayoutManager(lm);
+        myTripsView.setAdapter(adapter);
+        adapter.setClickListener(this);
+    }
+
+    private List<Trip> htmlGetTrips(){
+        List<Trip> trips = new ArrayList<Trip>();
+        trips.add(new Trip("Montreal", "Toronto", 200, true, new Time(1000000), new Time(1000000000), 200, 3, new Date(1000000), false, null));
+        trips.add(new Trip("Montreal", "Quebec", 200, true, new Time(1000000), new Time(1000000000), 200, 3, new Date(1000000), false, null));
+        trips.add(new Trip("Montreal", "Toronto", 200, true, new Time(1000000), new Time(1000000000), 200, 3, new Date(1000000), false, null));
+        trips.add(new Trip("Montreal", "Quebec", 200, true, new Time(1000000), new Time(1000000000), 200, 3, new Date(1000000), false, null));
+        trips.add(new Trip("Montreal", "Toronto", 200, true, new Time(1000000), new Time(1000000000), 200, 3, new Date(1000000), false, null));
+        trips.add(new Trip("Montreal", "Quebec", 200, true, new Time(1000000), new Time(1000000000), 200, 3, new Date(1000000), false, null));
+        trips.add(new Trip("Montreal", "Toronto", 200, true, new Time(1000000), new Time(1000000000), 200, 3, new Date(1000000), false, null));
+        trips.add(new Trip("Montreal", "Quebec", 200, true, new Time(1000000), new Time(1000000000), 200, 3, new Date(1000000), false, null));
+        trips.add(new Trip("Montreal", "Toronto", 200, true, new Time(1000000), new Time(1000000000), 200, 3, new Date(1000000), false, null));
+        trips.add(new Trip("Montreal", "Quebec", 200, true, new Time(1000000), new Time(1000000000), 200, 3, new Date(1000000), false, null));
+        trips.add(new Trip("Montreal", "Toronto", 200, true, new Time(1000000), new Time(1000000000), 200, 3, new Date(1000000), false, null));
+        trips.add(new Trip("Montreal", "Quebec", 200, true, new Time(1000000), new Time(1000000000), 200, 3, new Date(1000000), false, null));
+        trips.add(new Trip("Montreal", "Toronto", 200, true, new Time(1000000), new Time(1000000000), 200, 3, new Date(1000000), false, null));
+        trips.add(new Trip("Montreal", "Quebec", 200, true, new Time(1000000), new Time(1000000000), 200, 3, new Date(1000000), false, null));
+        trips.add(new Trip("Montreal", "Toronto", 200, true, new Time(1000000), new Time(1000000000), 200, 3, new Date(1000000), false, null));
+        trips.add(new Trip("Montreal", "Quebec", 200, true, new Time(1000000), new Time(1000000000), 200, 3, new Date(1000000), false, null));
+        trips.add(new Trip("Montreal", "Toronto", 200, true, new Time(1000000), new Time(1000000000), 200, 3, new Date(1000000), false, null));
+        trips.add(new Trip("Montreal", "Quebec", 200, true, new Time(1000000), new Time(1000000000), 200, 3, new Date(1000000), false, null));
+        trips.add(new Trip("Montreal", "Toronto", 200, true, new Time(1000000), new Time(1000000000), 200, 3, new Date(1000000), false, null));
+        trips.add(new Trip("Montreal", "Quebec", 200, true, new Time(1000000), new Time(1000000000), 200, 3, new Date(1000000), false, null));
+        trips.add(new Trip("Montreal", "Toronto", 200, true, new Time(1000000), new Time(1000000000), 200, 3, new Date(1000000), false, null));
+        trips.add(new Trip("Montreal", "Quebec", 200, true, new Time(1000000), new Time(1000000000), 200, 3, new Date(1000000), false, null));
+
+        return trips;
+    }
+
+    @Override
+    public void onItemClick(View view, int position) {
+        Intent mainIntent = new Intent(this, editDeleteTripActivity.class);
+        startActivity(mainIntent);
+    }
 }
+
+
---
+++ Driver-app/app/src/main/java/ca/mcgill/ecse321/driverapp/model/Role.java
@@ -0,0 +1,6 @@
+package ca.mcgill.ecse321.driverapp.model;
+
+public enum Role{
+    PASSENGER,
+    DRIVER
+}
