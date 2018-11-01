6abf679e0a964a79e174851a0115fb804b2d4a7b
Jacob McConnell <jacob.mcconnell@mail.mcgill.ca>
2018-10-31 20:19:09 -0400

    passenger main page created but some kind of build error i don't understand

--- Passenger-app/app/src/main/java/ca/mcgill/ecse321/passengerapp/MainActivity.java
+++ Passenger-app/app/src/main/java/ca/mcgill/ecse321/passengerapp/MainActivity.java
@@ -2,6 +2,14 @@ package ca.mcgill.ecse321.passengerapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.content.Intent;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +17,29 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // dont know if this part of the code is necesary
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        //
     }
+
+    public void myTripBtnClick(View view){
+        //Changes view to main view
+        Intent myTripsIntent = new Intent(this, MyTripsActivity.class);
+        startActivity(myTripsIntent);
+
+    }
+
+
+    public void allTripsBtnClick(View view){
+        //Changes view to main view
+        Intent myTripsIntent = new Intent(this, AllTripsActivity.class);
+        startActivity(myTripsIntent);
+
+    }
+
+
+
 }
