551a4ae7e22d7e1009bfa52934b1aa4216486910
Aleksas Murauskas <aleksas.murauskas@mail.mcgill.ca>
2018-11-01 22:17:38 -0400

    Update to created trip

--- Driver-app/app/src/main/java/ca/mcgill/ecse321/driverapp/CreateTripActivity.java
+++ Driver-app/app/src/main/java/ca/mcgill/ecse321/driverapp/CreateTripActivity.java
@@ -4,10 +4,27 @@ import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.sql.Time;
+import java.util.Date;
+
+import ca.mcgill.ecse321.driverapp.model.Vehicle;
 
 public class CreateTripActivity extends AppCompatActivity {
+
+    private EditText DestinationInput;
+    private EditText LocationInput;
+    private EditText DateInput;
+    private EditText TimeInput;
+    private RecyclerView VehicleInput;
+    private RecyclerView TripnodeInput;
+    private TextView errorTxt;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,15 +32,38 @@ public class CreateTripActivity extends AppCompatActivity {
         setContentView(R.layout.activity_create_trip);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        DestinationInput =(EditText)findViewById(R.id.Destinationbox);
+        LocationInput =(EditText)findViewById(R.id.Locationbox);
+        DateInput = (EditText) findViewById(R.id.Datebox);
+        TimeInput = (EditText) findViewById((R.id.Timebox));//String then cast as a date object
+        VehicleInput = (RecyclerView) findViewById(R.id.Vehiclebox);
+        TripnodeInput = (RecyclerView) findViewById(R.id.TripNodeBox);
     }
+
+    public void createBtnClick(View view){
+
+    }
+
+    public void createVehiclebtnClick(View view){
+       String destination = DestinationInput.getText().toString();
+       String location = LocationInput.getText().toString();
+       String timeString = TimeInput.getText().toString();
+       String dateString = TimeInput.getText().toString();
+
+      //Somehow create this here  Vehicle vehicle = VehicleInput.
+
+        if(destination == ""){
+            errorTxt.setText("Unable to login: Please input a password");
+        }
+    }
+
+    private void createTripNodebtn(View view){
+
+    }
+
+    public void createTrip(){
+
+    }
+
 
 }
